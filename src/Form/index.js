c7692ce6f6040911a88aff4aab4ae86985dc8616
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-02-18 17:49:58 +0100

    added onSubmit, rendered input with .map

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,16 @@
 import "./style.css"
 import currencies from "../Currencies";
+import { useState } from "react";
 
 const Form = () => {
+    const [amount, setAmount] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <div className="container">
-            <form className="form">
+            <form className="form" onSubmit={onSubmit}>
                 <fieldset className="form__fieldset">
                     <legend className="form__legend">Kantor wymiany walut</legend>
                     <p>
@@ -12,37 +18,42 @@ const Form = () => {
                             <span className="form__labelText">
                                 Podaj kwotę w zł:
                             </span>
-                            <input className="form__field" type="number" step="0.01" name="kwota_zl" placeholder="0.00"
-                                autofocus required min="0.01" />
+                            <input
+                                value={amount}
+                                onChange={({ target }) => setAmount(target.value)}
+                                className="form__field"
+                                type="number"
+                                step="0.01"
+                                name="kwota_zl"
+                                placeholder="0.00"
+                                autoFocus
+                                required
+                                min="0.01" />
                         </label>
                     </p>
                     <p>Wybierz walutę (kurs z dn. 18.02.2023):</p>
                     <ul className="form__labelList">
-                        <li>
-                            <label>
-                                <input type="radio" name="currency" value="5.35" checked /> £ funt
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type="radio" name="currency" value="4.40" /> $ dolar
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type="radio" name="currency" value="4.67" /> € euro
-                            </label>
-                        </li>
+                        {currencies.map((currency => (
+                            <li>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="currency"
+                                    />
+                                    {currency.name}
+                                </label>
+                            </li>
+                        )))}
                     </ul>
-                    <p>
-                        <label>
-                            Po wymianie otrzymasz: <span className="form__labelResult">0.00</span>
-                        </label>
-                    </p>
                     <p>
                         <button className="form__button">
                             Przelicz
                         </button>
+                    </p>
+                    <p>
+                        <label>
+                            Po wymianie otrzymasz: <span className="form__labelResult">0.00</span>
+                        </label>
                     </p>
                 </fieldset>
 
