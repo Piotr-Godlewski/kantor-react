e77e19b7b8ccd76dfc1e1733f78ef1bcbb571b45
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-03-05 21:26:30 +0100

    corrected code in Clock after checked homework

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -2,23 +2,31 @@ import { useEffect, useState } from "react"
 import "./style.css"
 
 const Clock = () => {
-const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
-useEffect(() => {
-    const intervalId = setInterval(() => {
-        setDate(date => date = new Date());
-    }, 1000);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
 
-    return () => {
-        clearInterval(intervalId);
-    };
-}, []);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
 
-return (
-    <p className="form__date">
-        Dzisiaj jest {date.toLocaleDateString(undefined, {weekday:"long", day:"numeric", month:"long"})}, {date.toLocaleTimeString()}
-    </p>
-)
+    return (
+        <p className="form__date">
+            Dzisiaj jest
+            {date.toLocaleDateString(
+                undefined,
+                {
+                    weekday: "long",
+                    day: "numeric",
+                    month: "long"
+                })},
+            {date.toLocaleTimeString()}
+        </p>
+    )
 }
 
 export default Clock
