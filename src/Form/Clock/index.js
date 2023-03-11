796872112da1d81f306391a284eea7f9a921dbfe
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-03-11 16:34:40 +0100

    changed styl.css to styled-components styled.js

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import "./style.css"
+import { StyledClock } from "./styled";
 
 const Clock = () => {
     const [date, setDate] = useState(new Date());
@@ -15,7 +15,7 @@ const Clock = () => {
     }, []);
 
     return (
-        <p className="form__date">
+        <StyledClock>
             Dzisiaj jest
             {date.toLocaleDateString(
                 undefined,
@@ -25,7 +25,7 @@ const Clock = () => {
                     month: "long"
                 })},
             {date.toLocaleTimeString()}
-        </p>
+        </StyledClock>
     )
 }
 
--- src/Form/Clock/styled.js
+++ src/Form/Clock/styled.js
@@ -1,6 +1,8 @@
-.form__date{
+import styled from "styled-components";
+
+export const StyledClock = styled.p`
     text-align: right;
     font-size: 11px;
     font-family:'Courier New', Courier, monospace;
     font-weight: bold;
-}
+`;
