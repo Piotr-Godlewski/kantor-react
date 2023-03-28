eb65b29d0a82679d85cf967d5c449c1b1bbf56b8
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-03-28 21:15:55 +0200

    added messages "wait" and "error"

--- src/Form/index.js
+++ src/Form/index.js
@@ -9,6 +9,7 @@ const Form = ({ calculateResult, result }) => {
     const [amount, setAmount] = useState("");
     const [currency, setCurrency] = useState(currencies[0].symbol);
     const ratesData = useRatesData();
+    console.log(ratesData);
 
     const onSelectChange = ({ target }) => setCurrency(target.value);
 
@@ -18,6 +19,11 @@ const Form = ({ calculateResult, result }) => {
         calculateResult(amount, currency);
     }
 
+    if (ratesData.error) {
+        return (
+            <FormContainer> BŁĄD !!! </FormContainer>
+        )
+    }
     return (
         <FormContainer>
             <StyledForm onSubmit={onSubmit}>
@@ -25,48 +31,53 @@ const Form = ({ calculateResult, result }) => {
                 <Header>
                     Kantor wymiany walut
                 </Header>
-                <Paragraph>
-                    <LabelText>
-                        Podaj kwotę w zł:
-                    </LabelText>
-                    <FormField
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        type="number"
-                        step="0.01"
-                        name="kwota_zl"
-                        placeholder="0.00"
-                        autoFocus
-                        required
-                        min="0.01"
-                    />
-                </Paragraph>
-                <Paragraph>
-                    <LabelText>
-                        Wybierz walutę:
-                    </LabelText>
-                    <FormField as={"select"}
-                        value={currency}
-                        onChange={onSelectChange}
-                    >
-                        {currencies.map((currency => (
-                            <option
-                                key={currency.symbol}
-                                value={currency.symbol}
+                {ratesData.loading ?
+                    <Header> Poczekaj Chwileczkę 😍</Header> :
+                    <>
+                        <Paragraph>
+                            <LabelText>
+                                Podaj kwotę w zł:
+                            </LabelText>
+                            <FormField
+                                value={amount}
+                                onChange={({ target }) => setAmount(target.value)}
+                                type="number"
+                                step="0.01"
+                                name="kwota_zl"
+                                placeholder="0.00"
+                                autoFocus
+                                required
+                                min="0.01"
+                            />
+                        </Paragraph>
+                        <Paragraph>
+                            <LabelText>
+                                Wybierz walutę:
+                            </LabelText>
+                            <FormField as={"select"}
+                                value={currency}
+                                onChange={onSelectChange}
                             >
-                                {currency.symbol}
-                            </option>
-                        )))};
-                    </FormField>
-                </Paragraph>
-                <Button>
-                    Przelicz
-                </Button>
-                <Result result={result} />
-                <Footer>
-                    Kursy walut zostały pobrane z Europejskiego Banku Centralnego
-                    <br /> Aktualne na dzień: {ratesData.date}
-                </Footer>
+                                {Object.keys(ratesData.rates).map((currency => (
+                                    <option
+                                        key={currency}
+                                        value={currency}
+                                    >
+                                        {currency}
+                                    </option>
+                                )))};
+                            </FormField>
+                        </Paragraph>
+                        <Button>
+                            Przelicz
+                        </Button>
+                        <Result result={result} />
+                        <Footer>
+                            Kursy walut zostały pobrane z Europejskiego Banku Centralnego
+                            <br /> Aktualne na dzień: {ratesData.date}
+                        </Footer>
+                    </>
+                }
             </StyledForm>
 
         </FormContainer>
