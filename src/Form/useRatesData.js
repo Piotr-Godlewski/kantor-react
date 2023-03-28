b7bdbdef2ace54dbcb518f3524f03d9bec70437d
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-03-28 21:14:39 +0200

    changed code form fetch to axios

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const useRatesData = () => {
-    const [ratesData, setRatesData] = useState();
-    
+    const [ratesData, setRatesData] = useState({ loading: true });
+
     useEffect(() => {
-        fetch("https://api.exchangerate.host/latest?base=PLN")
-        .then(response => response.json())
-        .then(setRatesData)   
+        const axiosRates = async () => {
+            try {
+                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                setRatesData({
+                    date: response.data.date,
+                    rates: response.data.rates
+                });
+            }
+            catch (error) {
+                console.error("masz błąd", error);
+                setRatesData({
+                    error
+                });
+            }
+        };
+
+        setTimeout(axiosRates, 1500);
     }, [])
 
     return ratesData
