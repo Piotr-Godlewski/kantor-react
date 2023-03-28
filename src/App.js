d93e9620e0107e490dfbef7326b5180a0532160c
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-03-28 21:10:42 +0200

    import data from useRatesData (API)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Form from './Form';
-import currencies from './currencies'
 import { useState } from 'react';
+import { useRatesData } from './Form/useRatesData';
 
 function App() {
   const [result, setResult] = useState();
+  const ratesData = useRatesData();
 
   const calculateResult = (amount, currency) => {
-    const rate = currencies.find(({ symbol }) => symbol === currency).rate;
+    const rate = ratesData.rates[currency];
 
     setResult({
       sourceAmount: + amount,
-      targetResult: amount / rate,
+      targetResult: amount * rate,
       currency
     })
   }
