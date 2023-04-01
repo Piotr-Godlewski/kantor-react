d9282655706eeab934bcdd98824d03277da92899
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-04-01 14:44:08 +0200

    refactroing code

--- src/useRatesData.js
+++ src/useRatesData.js
@@ -8,13 +8,15 @@ export const useRatesData = () => {
         const axiosRates = async () => {
             try {
                 const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+
                 setRatesData({
                     date: response.data.date,
                     rates: response.data.rates
                 });
             }
             catch (error) {
-                console.error("masz błąd", error);
+                console.error(error);
+
                 setRatesData({
                     error
                 });
@@ -22,7 +24,7 @@ export const useRatesData = () => {
         };
 
         setTimeout(axiosRates, 1500);
-    }, [])
+    }, []);
 
     return ratesData
 }
