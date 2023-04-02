138b8b6b6c49f4717fe523e1cb74e56fc37c3a94
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-04-02 14:37:22 +0200

    added API_URL and chaging name async function

--- src/useRatesData.js
+++ src/useRatesData.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const API_URL = "https://api.exchangerate.host/latest?base=PLN"
+
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState({ loading: true });
 
     useEffect(() => {
-        const axiosRates = async () => {
+        const getRates = async () => {
             try {
-                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                const response = await axios.get(API_URL);
 
                 setRatesData({
                     date: response.data.date,
@@ -23,7 +25,7 @@ export const useRatesData = () => {
             }
         };
 
-        setTimeout(axiosRates, 1500);
+        setTimeout(getRates, 1500);
     }, []);
 
     return ratesData
